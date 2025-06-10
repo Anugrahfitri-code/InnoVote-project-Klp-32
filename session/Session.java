cba36783ce2abb3461a360408c4ef00831cdf67c
HanifahBasir <hanifahatthahirahbasir@gmail.com>
2025-06-10 12:02:20 +0800

    add Session class to store logged-in user data

--- session/Session.java
+++ session/Session.java
@@ -1,1 +1,23 @@
+package session;
 
+import models.User; 
+
+public class Session {
+    private static User currentUser;
+
+    public static void login(User user) {
+        currentUser = user;
+    }
+
+    public static void logout() {
+        currentUser = null;
+    }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static boolean isLoggedIn() {
+        return currentUser != null;
+    }
+}
