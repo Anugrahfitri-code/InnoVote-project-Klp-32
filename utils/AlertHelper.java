6a1c6607f6d4f0eae0402a11d2aecc121e3d5580
HanifahBasir <hanifahatthahirahbasir@gmail.com>
2025-06-10 12:03:25 +0800

    add AlertHelper for showing alerts

--- utils/AlertHelper.java
+++ utils/AlertHelper.java
@@ -1,1 +1,13 @@
+package utils;
 
+import javafx.scene.control.Alert; 
+
+public class AlertHelper {
+    public static void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null); 
+        alert.setContentText(message);
+        alert.showAndWait(); 
+    }
+}
