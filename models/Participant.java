1d4405b9c0b21bb7b82b5fd84e448d347f19a05b
HanifahBasir <hanifahatthahirahbasir@gmail.com>
2025-06-10 11:57:09 +0800

    add Participant class extending User

--- models/Participant.java
+++ models/Participant.java
@@ -1,1 +1,22 @@
+package models;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;  
+
+public class Participant extends User {
+    private List<Idea> submittedIdeas;
+
+    public Participant(String id, String username, String password) {
+        super(id, username, password, "Participant");
+        this.submittedIdeas = new ArrayList<>();
+    }
+
+    public void submitIdea(Idea idea) {
+        this.submittedIdeas.add(idea);
+    }
+
+    public List<Idea> getSubmittedIdeas() {
+        return new ArrayList<>(submittedIdeas);
+    }
+}
