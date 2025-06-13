9bc4336b41d295288772df98d392119a4d01a114
Ira Fitrinovanda <irafitrinovanda@gmail.com>
2025-06-13 20:55:35 +0800

    feat: menambahkan main.java

--- app/MainApp.java
+++ app/MainApp.java
@@ -1,1 +1,20 @@
+package com.innovote.app;
 
+import com.innovote.screens.auth.LoginScreen;
+import com.innovote.utils.SceneManager;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        SceneManager.setPrimaryStage(primaryStage); 
+        SceneManager.switchToScreen(new LoginScreen());
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
